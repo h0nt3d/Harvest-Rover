ee76b3b67133d6be3ce2fa0824fd17e4506fb06e
h0nt3d <akanoim@gmail.com>
2026-02-07 22:45:33 -0400

    Added test for motor settings

--- pcu.c
+++ pcu.c
@@ -1,8 +1,11 @@
 #include <xc.h>
 #include "init.h"
 
+#define _XTAL_FREQ 4000000
+
 volatile uint8_t get_pcu_info[6] = {0xFE, 0x19, 0x01, 0x04, 0x00, 0x00};
 volatile uint8_t get_flySky_info[6] = {0xFE, 0x19, 0x01, 0x05, 0x00, 0x00};
+volatile uint8_t set_motor_settings[10] = {0xFE, 0x19, 0x01, 0x06, 0x04, 0x00, 0x01, 80, 0x01, 80};
 
 void send_get_pcu_info()
 {
@@ -16,7 +19,15 @@ void send_get_flySky_info()
 {
     for (int i = 0; i < 6; i++) {
         TX1REG = get_flySky_info[i];
-        while (!TX1STAbits.TRMT) {} // wait until register is empty
+        while (!TX1STAbits.TRMT) {}
+    }
+}
+
+void send_set_motor_settings()
+{
+    for (int i = 0; i < 10; i++) {
+        TX1REG = set_motor_settings[i];
+        while (!TX1STAbits.TRMT) {}
     }
 }
 
@@ -35,10 +46,10 @@ void __interrupt() ISR()
         rxDone = 0;
         
         //send_get_pcu_info();
-        send_get_flySky_info();
+        send_set_motor_settings();
         IOCAFbits.IOCAF5 = 0;  // Clear IOC flag
     } 
-    
+
     if (PIE3bits.RCIE && PIR3bits.RCIF) {
         // handle overrun
         if (RC1STAbits.OERR) {
