fb0077930f96f788b193126fa27cdcd1c9086cb1
h0nt3d <akanoim@gmail.com>
2026-02-20 21:58:27 -0400

    Added left and right direction

--- init.h
+++ init.h
@@ -5,6 +5,7 @@
 
 #define DEADZONE 40
 #define RJ_Y_CENTER 0x05DD
+#define LJ_Y_CENTER 0x05DD
 #define MAX_FWD_OFFSET 500
 
 
--- main.c
+++ main.c
@@ -17,34 +17,66 @@ void main()
         uint8_t rjy_lsb = get_flySky_info_buf[8];
         uint8_t rjy_msb = get_flySky_info_buf[9];
         
+        uint8_t ljy_lsb = get_flySky_info_buf[12];
+        uint8_t ljy_msb = get_flySky_info_buf[13];
+        
         uint16_t RJY = (uint16_t)rjy_lsb | ((uint16_t)rjy_msb << 8);
-        int16_t offset = (int16_t)RJY - (int16_t)RJ_Y_CENTER;
+        uint16_t LJY = (uint16_t)ljy_lsb | ((uint16_t)ljy_msb << 8);
+        
+        int16_t offset_rj = (int16_t)RJY - (int16_t)RJ_Y_CENTER;
+        int16_t offset_lj = (int16_t)LJY - (int16_t)LJ_Y_CENTER;
         
         uint8_t pwm = 0;
         
         // Forward
-        if (offset > DEADZONE) {
-            offset -= DEADZONE;
+        if (offset_rj > DEADZONE) {
+            offset_rj -= DEADZONE;
             
-            if (offset > MAX_FWD_OFFSET)
-                offset = MAX_FWD_OFFSET;
+            if (offset_rj > MAX_FWD_OFFSET)
+                offset_rj = MAX_FWD_OFFSET;
             
-            pwm = (uint8_t)((offset * 100) / MAX_FWD_OFFSET);
+            pwm = (uint8_t)((offset_rj * 100) / MAX_FWD_OFFSET);
             
             send_motor_settings(1, pwm, 1, pwm);
         }
         
         // Backward
-        else if (offset < -DEADZONE) {
-            offset = -offset;
-            offset += DEADZONE;
+        else if (offset_rj < -DEADZONE) {
+            offset_rj = -offset_rj;
+            offset_rj += DEADZONE;
             
-            if (offset > MAX_FWD_OFFSET)
-                offset = MAX_FWD_OFFSET;
+            if (offset_rj > MAX_FWD_OFFSET)
+                offset_rj = MAX_FWD_OFFSET;
             
-            pwm = (uint8_t)((offset * 100) / MAX_FWD_OFFSET);
+            pwm = (uint8_t)((offset_rj * 100) / MAX_FWD_OFFSET);
             
             send_motor_settings(2, pwm, 2, pwm);
+        }
+        
+        //Right
+        else if (offset_lj > DEADZONE) {
+            offset_lj -= DEADZONE;
+            
+            if (offset_lj > MAX_FWD_OFFSET)
+                offset_lj = MAX_FWD_OFFSET;
+            
+            pwm = (uint8_t)((offset_lj * 100) / MAX_FWD_OFFSET);
+            
+            send_motor_settings(1, pwm, 2, pwm);
+        }
+        
+        else if (offset_lj < -DEADZONE) {
+            /*offset_lj -= DEADZONE;*/
+            offset_lj = -offset_lj;
+            offset_lj += DEADZONE;
+            
+            
+            if (offset_lj > MAX_FWD_OFFSET)
+                offset_lj = MAX_FWD_OFFSET;
+            
+            pwm = (uint8_t)((offset_lj * 100) / MAX_FWD_OFFSET);
+            
+            send_motor_settings(2, pwm, 1, pwm);
         }
         
         // Stop
